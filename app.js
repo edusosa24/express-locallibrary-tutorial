a28e806696ca4f1b4281845b9252e21b3fbc334e
Eduardo Sosa <edu_33@live.com.ar>
2022-03-14 13:23:28 -0300

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.use(compression()); //Compress all routes
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://smeduardo:eduardodb@cluster0.a61oo.mongodb.net/local_library?retryWrites=true&w=majority';
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://smeduardo:eduardodb@cluster0.a61oo.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
